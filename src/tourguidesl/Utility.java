2f6c7955d04e088e2f04f07d32b5f63eb81b7db5
NiTR0-CH4RG3R <buddhimakaveeshwara@gmail.com>
2023-05-26 01:05:57 +0530

    Added StringToVehicleType() function that converts a string to the relevant VehicleType enum value.

--- src/tourguidesl/Utility.java
+++ src/tourguidesl/Utility.java
@@ -21,4 +21,11 @@ public class Utility {
     static LocalDateTime StringToLocalDateTime( final String str ) {
         return LocalDateTime.parse( str, DateTimeFormatter.ofPattern( "yyyy-MM-dd HH:mm:ss" ) );
     }
+    
+    static VehicleType StringToVehicleType( final String str ) {
+        for ( var e : VehicleType.values() ) {
+            if ( str.equals( e.toString() ) ) return e;
+        }
+        return VehicleType.UNKNOWN;
+    }
 }
